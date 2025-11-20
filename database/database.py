7b7378c6bca9d9b238cbeced647b232d0a8a7852
hartake <hatakeofficial@gmail.com>
2025-11-19 18:52:36 -0700

    fix: make database host configurable via env var

--- database/database.py
+++ database/database.py
@@ -13,8 +13,9 @@ DB_USER = os.getenv("DB_USER")
 DB_PASSWORD = os.getenv("DB_PASSWORD")
 DB_NAME = os.getenv("DB_NAME")
 DB_PORT = os.getenv("DB_PORT")
+DB_HOST = os.getenv("DB_HOST", "localhost")
 
-DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@localhost:{DB_PORT}/{DB_NAME}"
+DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
 cached_db_pool = None
 
